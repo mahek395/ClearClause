84478e1fb82d98e99ee204a7596eb911aeb7754e
Mahek Shah <mahekshah395@gmail.com>
2026-06-18 14:34:52 +0530

    fix: change bcrypt import to bcryptjs

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import pool from '../config/db.js';
 import { loginLimiter, registerLimiter } from "../middleware/rateLimiter.js";
 import {
